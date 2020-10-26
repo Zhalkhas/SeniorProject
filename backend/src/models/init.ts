8ff3c1a80006690d1af243aa39aa5d203f6f1790
Zhalkhas <jalgas1998@gmail.com>
2020-10-26 22:24:47 +0000

    Created migrations for User model

--- backend/src/models/init.ts
+++ backend/src/models/init.ts
@@ -3,6 +3,7 @@ import {Sequelize} from 'sequelize';
 import {init as initGroupModel} from './Group.model';
 import {init as initUserModel} from './User.model';
 import {init as initPersonModel} from './Person.model';
+import userMigrations from '../migrations/User.migrations';
 
 export default async (db: Sequelize) => {
     initUserModel(db);
@@ -10,4 +11,6 @@ export default async (db: Sequelize) => {
     initPersonModel(db);
 
     await db.sync({force: true, alter: true});
+
+    await userMigrations();
 };
