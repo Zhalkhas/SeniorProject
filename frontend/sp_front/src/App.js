2ae9686ff3cf9a9213ba0917e9db5ae5aeaa6d31
Ablan Abkenov <abkenov.ablan@gmail.com>
2020-10-27 18:04:01 +0600

    Init: Authorization component

---
+++ frontend/sp_front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Authorization from './Components/Authorization.js'
+
+
+function App() {
+  return (
+    <div>
+      <Authorization />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/sp_front/src/Components/Authorization.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+const baseURL = "http://localhost:8888"
+
+function Authorization() {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    /*useEffect(() => {
+        
+    }, [])*/
+
+    function submitForm(e) {
+        e.preventDefault()
+
+        const newURL = baseURL + "/api/auth/login?username=admin&password=admin"
+        console.log(newURL)
+
+        axios.get(newURL, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            proxy: {
+                host: '172.17.0.1',
+                port: 8888
+            }
+        })
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+        console.log({username})
+        console.log({password})
+    }
+
+    return (
+        <div className="Auth">
+            <div>
+                <form onSubmit={submitForm}>
+                    <input type="text" id="username" onChange={e => setUsername(e.target.value)} placeholder="Username"></input>
+                    <input type="password" id="password" onChange={e => setPassword(e.target.value)} placeholder="Password"></input>
+                    <input type="submit"></input>
+                </form>
+            </div>
+        </div>
+    ) 
+}
+
+export default Authorization
