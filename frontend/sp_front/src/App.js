5035b7d103dd22328cce534828fdf6d0dacacb0c
Ablan Abkenov <abkenov.ablan@gmail.com>
2021-02-05 12:36:19 +0600

    added scheduler and table libraries

--- frontend/sp_front/src/App.js
+++ frontend/sp_front/src/App.js
@@ -6,22 +6,26 @@ import Cameras from './components/Cameras.js'
 import Groups from './components/Groups.js'
 import Actions from './components/Actions.js'
 import EditCamera from './components/EditCamera.js'
+import EditAction from './components/EditAction.js'
+import EditRulesForPeople from './components/EditRulesForPeople.js'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Authorization} />
-        <Route path="/menu" exact component={Menu} />
-        <Route path="/persons" component={Persons} />
-        <Route path="/cameras" exact component={Cameras} />
-        <Route path="/groups" component={Groups} />
-        <Route path="/actions" component={Actions} />
-        <Route path="/cameras/edit" component={EditCamera} />
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Authorization} />
+                <Route path="/menu" exact component={Menu} />
+                <Route path="/persons" component={Persons} />
+                <Route path="/cameras" exact component={Cameras} />
+                <Route path="/groups" component={Groups} />
+                <Route path="/actions" component={Actions} />
+                <Route path="/cameras/edit" component={EditCamera} />
+                <Route path="/zones_and_rules/edit" component={EditRulesForPeople} />
+                <Route path="/table" component={EditAction} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
