e7a317495ceed49e8b4d9aafdac4a0b94b1e5de1
Zhalkhas <jalgas1998@gmail.com>
2021-02-24 23:31:09 +0000

    added camera controller and service

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -4,6 +4,7 @@ import initMiddlewares from './middlewares/init';
 
 import AuthController from './controllers/auth-controller';
 import PersonsController from './controllers/persons-controller';
+import CamerasController from './controllers/cameras-controller';
 
 const apiRootRouter = express.Router();
 
@@ -13,6 +14,7 @@ initMiddlewares(app);
 
 apiRootRouter.use('/auth', AuthController);
 apiRootRouter.use('/persons', PersonsController);
+apiRootRouter.use('/cameras', CamerasController);
 
 app.use('/api', apiRootRouter);
 
---
+++ backend/src/controllers/cameras-controller.ts
@@ -0,0 +1,65 @@
+import Router from 'express';
+
+import Camera from "../models/Camera.model";
+import CameraService from "../services/cameras-service";
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+    const cameras: Camera[] = await CameraService.getAllCameras();
+    res.json(cameras);
+});
+
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    if (!id) return res.sendStatus(400);
+    try {
+        const camera: Camera = await CameraService.getCameraByID(id);
+        res.json(camera);
+    } catch (err) {
+        console.error(err);
+        res.status(400).send(err.message);
+    }
+});
+
+router.post('/', async (req, res) => {
+    const personData = req.body;
+    try {
+        const result = await CameraService.createCamera(personData);
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(400).send(err.message);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+
+    if (!id) return res.sendStatus(400);
+
+    const cameraData = req.body;
+    try {
+        const result = await CameraService.updateCamera(id, cameraData);
+        res.json(result);
+    } catch (err) {
+        console.error(err);
+        res.status(400).send(err.message);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+
+    if (!id) return res.sendStatus(400);
+
+    try {
+        await CameraService.deleteCamera(id);
+        res.sendStatus(200);
+    } catch (err) {
+        console.error(err);
+        res.status(400).send(err.message);
+    }
+});
+
+export default router;
