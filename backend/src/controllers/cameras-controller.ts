0729e8a5eaee8b98d77d74bcbbfc43b4d6d3ea1a
Daulet Amirkhanov <damirkhanov01@gmail.com>
2021-02-25 06:33:02 +0100

    Update backend/src/controllers/cameras-controller.ts

--- backend/src/controllers/cameras-controller.ts
+++ backend/src/controllers/cameras-controller.ts
@@ -25,7 +25,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     const cameraData = req.body;
     try {
-        const result = await CameraService.createCamera(personData);
+        const result = await CameraService.createCamera(cameraData);
         res.json(result);
     } catch (err) {
         console.error(err);
