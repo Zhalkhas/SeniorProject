19966a4dbb44074421822f7ecad0a5a3d0e2ad94
Zhalkhas <37871005+Zhalkhas@users.noreply.github.com>
2021-03-14 08:03:36 +0000

    added auth request to back (#27)

--- frontend/next-js-frontend-app/pages/index.js
+++ frontend/next-js-frontend-app/pages/index.js
@@ -1,60 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
-import { useRouter } from 'next/router';
-// import { Redirect } from 'react-router-dom'
+import {useRouter} from 'next/router';
 
 const baseURL = 'http://localhost:8888';
 
 function Authorization() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [auth, setAuth] = useState(false);
-  const router = useRouter();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    // const [auth, setAuth] = useState(false);
+    const router = useRouter();
 
-  function submitForm(e) {
-    e.preventDefault();
+    function submitForm(e) {
+        e.preventDefault();
 
-    const URL = baseURL + `/api/auth/login?username=${username}&password=${password}`;
+        const URL = baseURL + `/api/auth/login?username=${username}&password=${password}`;
 
-    // axios.get(URL)
-    //     .then(response => {
-    //         console.log(response)
-
-    //         setAuth(true)
-
-    //         console.log(auth);
-    //     })
-    //     .catch(error => {
-    //         console.log(error)
-    //     })
-
-    if (username === 'admin' && password === 'admin') {
-      setAuth(true);
+        axios.get(URL)
+            .then(response => {
+                console.log(response);
+                let auth = response.status === 200;
+                if (auth) {
+                    router.push('/menu');
+                } else {
+                    alert("Invalid login or password");
+                }
+                console.log(auth)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
-  }
 
-  return (
-    <div className='auth'>
-      <form onSubmit={submitForm}>
-        <h1>Authorization</h1>
-        <input type='text' id='username' onChange={(e) => setUsername(e.target.value)} placeholder='Username'></input>
-        <input
-          type='password'
-          id='password'
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder='Password'
-        ></input>
-        <button
-          type='submit'
-          onClick={() => {
-            router.push('/menu');
-          }}
-        >
-          Log in
-        </button>
-      </form>
-    </div>
-  );
+    return (
+        <div className='auth'>
+            <form onSubmit={submitForm}>
+                <h1>Authorization</h1>
+                <input type='text' id='username' onChange={(e) => setUsername(e.target.value)}
+                       placeholder='Username'/>
+                <input
+                    type='password'
+                    id='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder='Password'
+                />
+                <button
+                    type='submit'
+                    // onClick={() => {
+                    //     if (auth) {
+                    //         router.push('/menu');
+                    //     } else {
+                    //         alert("Invalid login or password");
+                    //     }
+                    // }}
+                >
+                    Log in
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default Authorization;
