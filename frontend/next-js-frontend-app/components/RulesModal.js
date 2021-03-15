9e30a34a7da3cd6c3fdd00955c9dfbb12a26ea1e
Ablan Abkenov <abkenov.ablan@gmail.com>
2021-03-15 18:26:20 +0600

    auth alert fixed

--- frontend/next-js-frontend-app/components/RulesModal.js
+++ frontend/next-js-frontend-app/components/RulesModal.js
@@ -2,90 +2,84 @@ import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 
-function RulesModal({data, ...props}) {
-
+function RulesModal({ data, ...props }) {
   const [localData, setLocalData] = useState(data);
 
   const { days } = localData;
 
-  function CheckBox({day, isActive}) {
-      function toggleDayCheckbox(day) {
-          const newDays = [...days];
+  function CheckBox({ day, isActive }) {
+    function toggleDayCheckbox(day) {
+      const newDays = [...days];
 
-          const theDay = newDays.find(d => d.day === day);
+      const theDay = newDays.find((d) => d.day === day);
 
-          theDay.isActive = !theDay.isActive;
+      theDay.isActive = !theDay.isActive;
 
-          setLocalData({
-              ...localData,
-              days: newDays,
-          });
-      }
+      setLocalData({
+        ...localData,
+        days: newDays,
+      });
+    }
 
     return (
-        <div>
-            <label htmlFor={day}>{ day }</label>
-                <input type='checkbox' name={day} checked={isActive}
-                onClick={() => toggleDayCheckbox(day)}
-            />
-        </div>
+      <div>
+        <label htmlFor={day}>{day}</label>
+        <input type='checkbox' name={day} checked={isActive} onClick={() => toggleDayCheckbox(day)} />
+      </div>
     );
   }
 
   function fromToTime() {
-      function setTimeFrom(newTimeFrom) {
-          setLocalData({
-              ...localData,
-              timeFrom: newTimeFrom,
-          })
-      }
+    function setTimeFrom(newTimeFrom) {
+      setLocalData({
+        ...localData,
+        timeFrom: newTimeFrom,
+      });
+    }
 
-      function setTimeTo(newTimeTo) {
-          setLocalData({
-              ...localData,
-              timeTo: newTimeTo,
-          });
-      }
+    function setTimeTo(newTimeTo) {
+      setLocalData({
+        ...localData,
+        timeTo: newTimeTo,
+      });
+    }
 
-      const [timeFrom, timeTo] = [localData.timeFrom, localData.timeTo];
+    const [timeFrom, timeTo] = [localData.timeFrom, localData.timeTo];
 
-      return (
-          <div className='modal_time'>
-              <label htmlFor='time_from'>From: </label>
-              <input type='time' value={timeFrom} onChange={e => setTimeFrom(e.target.value)}/>
-              <label htmlFor='time_to'>To: </label>
-              <input type='time' value={timeTo} onChange={e => setTimeTo(e.target.value)}/>
-          </div>
-      );
+    return (
+      <div className='modal_time'>
+        <label htmlFor='time_from'>From: </label>
+        <input type='time' value={timeFrom} onChange={(e) => setTimeFrom(e.target.value)} />
+        <label htmlFor='time_to'>To: </label>
+        <input type='time' value={timeTo} onChange={(e) => setTimeTo(e.target.value)} />
+      </div>
+    );
   }
 
   function saveModal() {}
 
   return (
-      <Modal isOpen={props.modal} toggle={props.toggle} contentClassName={'custom_modal_styles'}>
-        <ModalHeader toggle={props.toggle}>Rules schedule</ModalHeader>
-        <ModalBody>
+    <Modal isOpen={props.modal} toggle={props.toggle} contentClassName={'custom_modal_styles'}>
+      <ModalHeader toggle={props.toggle}>Rules schedule</ModalHeader>
+      <ModalBody>
+        {days.map(({ day, isActive }) =>
+          CheckBox({
+            day,
+            isActive,
+          })
+        )}
 
-          {
-            days.map(
-                ({ day, isActive }) => CheckBox({
-                  day,
-                  isActive,
-                })
-            )
-          }
-
-            { fromToTime() }
-        </ModalBody>
-        <ModalFooter>
-          <Button className={'save'} onClick={saveModal}>
-            Save
-          </Button>{' '}
-          <Button color='secondary' onClick={props.toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
-      </Modal>
+        {fromToTime()}
+      </ModalBody>
+      <ModalFooter>
+        <Button className={'save'} onClick={saveModal}>
+          Save
+        </Button>{' '}
+        <Button color='secondary' onClick={props.toggle}>
+          Cancel
+        </Button>
+      </ModalFooter>
+    </Modal>
   );
 }
 
--- frontend/next-js-frontend-app/pages/index.js
+++ frontend/next-js-frontend-app/pages/index.js
@@ -1,63 +1,60 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 
 const baseURL = 'http://localhost:8888';
 
 function Authorization() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    // const [auth, setAuth] = useState(false);
-    const router = useRouter();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  // const [auth, setAuth] = useState(false);
+  const router = useRouter();
 
-    function submitForm(e) {
-        e.preventDefault();
+  let auth;
 
-        const URL = baseURL + `/api/auth/login?username=${username}&password=${password}`;
+  function submitForm(e) {
+    e.preventDefault();
 
-        axios.get(URL)
-            .then(response => {
-                console.log(response);
-                let auth = response.status === 200;
-                if (auth) {
-                    router.push('/menu');
-                } else {
-                    alert("Invalid login or password");
-                }
-                console.log(auth)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
+    const URL = baseURL + `/api/auth/login?username=${username}&password=${password}`;
 
-    return (
-        <div className='auth'>
-            <form onSubmit={submitForm}>
-                <h1>Authorization</h1>
-                <input type='text' id='username' onChange={(e) => setUsername(e.target.value)}
-                       placeholder='Username'/>
-                <input
-                    type='password'
-                    id='password'
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder='Password'
-                />
-                <button
-                    type='submit'
-                    // onClick={() => {
-                    //     if (auth) {
-                    //         router.push('/menu');
-                    //     } else {
-                    //         alert("Invalid login or password");
-                    //     }
-                    // }}
-                >
-                    Log in
-                </button>
-            </form>
-        </div>
-    );
+    axios
+      .get(URL)
+      .then((response) => {
+        console.log(response);
+        auth = response.status === 200;
+
+        if (auth) {
+          router.push('/menu');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+
+        alert('Invalid login or password \n\nHint: try to pretend being admin');
+      });
+  }
+
+  return (
+    <div className='auth'>
+      <form onSubmit={submitForm}>
+        <h1>Authorization</h1>
+        <input type='text' id='username' onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+        <input type='password' id='password' onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+        <button
+          type='submit'
+          // onClick={() => {
+          //     if (auth) {
+          //         router.push('/menu');
+          //     } else {
+          //         alert("Invalid login or password");
+          //     }
+          // }}
+        >
+          Log in
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Authorization;
